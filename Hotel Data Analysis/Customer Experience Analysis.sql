08f754822b787b8ca8108867dc7f9145a6531555
Divine Jude <judedivine68@gmail.com>
2023-05-29 14:36:37 +0100

    Update Customer Experience Analysis.sql

--- Hotel Data Analysis/Customer Experience Analysis.sql
+++ Hotel Data Analysis/Customer Experience Analysis.sql
@@ -13,19 +13,6 @@ LEFT JOIN
 	[meal_cost] ON hotels.meal = [meal_cost].meal;
 
 
--- calculates the total number of nights stayed by guest
-WITH hotels AS (
-SELECT * FROM [2018]
-UNION
-SELECT * FROM [2019]
-UNION
-SELECT * FROM [2020]
-)
-
-SELECT
-	SUM(stays_in_week_nights ) + SUM(stays_in_weekend_nights) AS Total_nights
-FROM hotels;
-
 
 -- calculate the number of bookings for each arrival year from the combined data of 2018, 2019, and 2020
 WITH hotels AS (
@@ -45,7 +32,7 @@ ORDER BY Number_of_booking DESC;
 
 
 
--- Does the customers require a car parking space? (0 — No, 1 — Yes)
+-- Does the customers require a car parking space? (0 â€” No, 1 â€” Yes)
 WITH hotels AS (
   SELECT * FROM [2018]
   UNION 
@@ -82,17 +69,3 @@ GROUP BY hotels.meal
 ORDER BY meal_count DESC;
 
 
--- calculates the number of bookings for each reserved room type
-WITH hotels AS (
-SELECT * FROM [2018]
-UNION
-SELECT * FROM [2019]
-UNION
-SELECT * FROM [2020]
-)
-
-SELECT reserved_room_type, 
-	COUNT(*) AS room_count
-FROM hotels
-GROUP BY reserved_room_type
-ORDER BY room_count DESC;
